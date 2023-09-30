b4c7b460253e095d24dc81efec9076214bfdc7d4
Nova <Nova@DESKTOP-M3SPKQP>
2023-09-30 18:32:33 +0300

    Добавьте файлы проекта.

---
+++ NewsResponse.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace ConsoleAppNewsapi
+{
+    public class NewsApiResponse
+    {
+        public List<Article> Articles { get; set; }
+
+        /// <summary>
+        /// Получение новостей из источника данных
+        /// </summary>
+        /// <param name="apiKey">Ключ</param>
+        /// <param name="query">Раздел</param>
+        /// <param name="LangSource">Язык (может быть пустым)</param>
+        /// <returns>Список новостей</returns>
+        public static async Task<IEnumerable<Article>> GetNewsAsync(string apiKey, string query, string LangSource)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "ConsoleAppNewsapi/1.0");
+                client.DefaultRequestHeaders.Add("x-api-key", apiKey);
+                string apiUrl = $"https://newsapi.org/v2/everything?q={query}&from=2023-09-10&sortBy=publishedAt{LangSource}&apiKey={apiKey}";
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    var news = JsonConvert.DeserializeObject<NewsApiResponse>(json);
+                    return news.Articles.Select(article => new Article
+                    {
+                        Author = article.Author,
+                        Title = article.Title,
+                        Description = article.Description,
+                        Content = article.Content
+                    });
+                }
+                else
+                {
+                    int statusCodeValue = (int)response.StatusCode;
+                    Console.WriteLine($"Ошибка при получении новостей: {response.ReasonPhrase}: {statusCodeValue}");
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Получение списка слов с наибольшим количеством гласных
+        /// </summary>
+        /// <param name="description">Исходный текст</param>
+        /// <param name="maxVowelCountValue">Максимальное количество гласных в слове(ах)</param>
+        /// <returns>Возвращает список слов и количество гласных</returns>
+        public static HashSet<string> GetWordsWithMostVowels(string description, out int maxVowelCountValue)
+        {
+            maxVowelCountValue = 0;
+            // Разбиваем текст новости на слова и находим слово с наибольшим количеством гласных
+            string[] separators = new string[] { " ", "  ", "\u00A0", ".", ",", ";", ":", "\"", "\'", "!", "?", "=", "\n", "\r" };
+
+
+            if (string.IsNullOrWhiteSpace(description))
+                return null;
+
+            string[] words = description.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            HashSet<string> wordsWithMostVowels = new HashSet<string>(); //Будем брать только уникальные
+
+            int maxVowelCount = 0;
+
+            foreach (string word in words)
+            {
+                int vowelCount = CountVowels(word);
+
+                if (vowelCount > maxVowelCount)
+                {
+                    maxVowelCount = vowelCount;
+                    wordsWithMostVowels.Clear(); // Очищаем список, так как нашли новое максимальное значение
+                    wordsWithMostVowels.Add(word);
+                }
+                else if (vowelCount == maxVowelCount)
+                {
+                    wordsWithMostVowels.Add(word); // Добавляем слово с таким же количеством гласных
+                }
+            }
+            maxVowelCountValue = maxVowelCount;
+
+            return wordsWithMostVowels;
+        }
+
+        /// <summary>
+        /// Подсчет количества гласных
+        /// </summary>
+        /// <param name="text">Текст</param>
+        /// <returns>Количество гласных</returns>
+        public static int CountVowels(string text)
+        {
+            string vowels = "AEIOUaeiouАЕЁИОУЭЮЯаеёиоуэюя";
+            return text.Count(c => vowels.Contains(c));
+        }
+    }
+    
+    public class Article
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ConsoleAppNewsapi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string defaultlangSource = "все языки";
+            string defaultApiKey = "27faf3aee9854015b5d934d8a475fdac";
+            string defaultQuery = "космос";
+
+            string query = ReadInput("Введите тему новостей", defaultQuery, false);
+            Console.WriteLine($"Выбрана тема новостей: {query}");
+
+            string apiKey = ReadApiKey("Введите apiKey", defaultApiKey);
+            Console.WriteLine($"Выбран apiKey: {apiKey.Substring(0, 5)}...{apiKey.Substring(apiKey.Length - 3)}");
+
+            string langSource = ReadInput("Выберите язык (ru, en)", defaultlangSource, false);
+            Console.WriteLine($"Выбран язык новостей: {langSource}");
+            langSource = (langSource == defaultlangSource) ? String.Empty : $"&language={langSource}";
+
+            await TextProcessor(apiKey, query, langSource);
+
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey(); 
+        }
+
+        /// <summary>
+        /// Производит работу с новостями
+        /// </summary>
+        /// <param name="apiKey">Ключ</param>
+        /// <param name="query">Раздел</param>
+        /// <param name="LangSource">Язык (может быть пустым)</param>
+        /// <returns></returns>
+        private static async Task TextProcessor(string apiKey, string query, string langSource)
+        {
+            //Получение новостей
+            var newsList = await NewsApiResponse.GetNewsAsync(apiKey, query, langSource);
+            if (newsList == null || newsList.Count() == 0)
+            {
+                Console.WriteLine($"Новости не найдены");
+                return;
+            }
+
+            //Обработка каждой новости
+            foreach (var news in newsList)
+            {
+                Console.WriteLine($"Новость: {news.Title}");
+                Console.WriteLine($"Автор: {news.Author}");
+                Console.WriteLine($"Описание: {news.Description.Trim()}");
+                // Console.WriteLine($"Содержание: {news.Content}");
+
+                //Получаем список слов с наибольшим количеством гласных
+                HashSet<string> wordsWithMostVowels = NewsApiResponse.GetWordsWithMostVowels(news.Description, out int maxVowelCount); //Будем брать только уникальные
+
+                if (wordsWithMostVowels == null || wordsWithMostVowels.Count() == 0)
+                    continue;
+
+                Console.WriteLine($"Слова с наибольшим количеством гласных ({maxVowelCount} гласных):");
+                foreach (string word in wordsWithMostVowels)
+                {
+                    Console.WriteLine(word);
+                }
+
+                Console.WriteLine(new string('-', 50));
+                await Task.Delay(500); //задержка просто так, для поэтапного вывода результата
+            }
+
+        }
+
+        /// <summary>
+        /// Обработка входных данных
+        /// </summary>
+        /// <param name="prompt">Сообщение</param>
+        /// <param name="defaultValue">Значение по умолчанию</param>
+        /// <param name="hide">Скрывать часть данных defaultValue</param>
+        /// <returns>Входной параметр</returns>
+        static string ReadInput(string prompt, string defaultValue, bool hide)
+        {
+            string defMess = hide ? $"{defaultValue.Substring(0, 5)}...{defaultValue.Substring(defaultValue.Length - 3)}" : defaultValue;
+            Console.Write($"{prompt} (по умолчанию {defMess}): ");
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                input = defaultValue;
+            }
+            return input;
+        }
+
+        /// <summary>
+        ///  Обработка входных данных, с дополнительной проверкой
+        /// </summary>
+        /// <param name="prompt">Сообщение</param>
+        /// <param name="defaultValue">Значение по умолчанию</param>
+        /// <returns>Входной параметр</returns>
+        static string ReadApiKey(string prompt, string defaultValue)
+        {
+            string input = ReadInput(prompt, defaultValue, true);
+
+            if (input.Length < 7)
+            {
+                Console.WriteLine("Некорректный apiKey, значение apiKey будет использовано по умолчанию.");
+                input = defaultValue;
+            }
+
+            return input;
+        }
+
+
+    }
+}
